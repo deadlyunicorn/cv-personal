0534c9ca721345503030a8181312b69d331d8598
deadlyunicorn <retroalex1008@gmail.com>
2023-05-13 22:28:39 +0300

    rearranged code

--- app/experience.tsx
+++ app/experience.tsx
@@ -1,15 +1,17 @@
 export default function ExperienceItem({skill,rating}:{skill:string,rating:string}){
 
   return(
-    <div>
-            <h2>{skill}</h2>
-            <div className='w-full h-4 rounded-md border border-[#a786df]'>
-              <div 
-              style={{
-                maxWidth:rating,
-              }} 
-              className='skillBar bg-gradient-to-l from-[#a786df] to-[#d8bfff] h-full rounded-md'></div>
-            </div>
-    </div>
+    <>
+      <h2>{skill}</h2>
+      <div className='w-full h-4 rounded-md border border-[#a786df]'>
+        <div 
+          className='skillBar bg-gradient-to-l from-[#a786df] to-[#d8bfff] h-full rounded-md'
+          style={{
+            maxWidth:rating,
+          }}>
+
+        </div>
+      </div>
+    </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="Yuni_transparent.png" type="image/x-icon" />
       </head>
-      <body className={ysabeau.className} style={{width:"100vw",display:"flex",justifyContent:"center",overflowX:"hidden",height:"1122px",maxHeight:"1123px"}}>{children}</body>
+      
+      <body className={ysabeau.className} style={{width:"100vw",display:"flex",justifyContent:"center",overflowX:"hidden",height:"1122px",maxHeight:"1123px"}}>
+        {children}
+      </body>
     </html>
   )
 }
