322ba50f295d9814d01b0ef3335ffbfaaf543a02
deadlyunicorn <retroalex1008@gmail.com>
2023-05-13 22:29:03 +0300

    rearranged code

--- app/links.tsx
+++ app/links.tsx
@@ -3,85 +3,85 @@ import Image from "next/image"
 
 const Links = () => (
 
-          <table className="text-base linkSection flex flex-col gap-2 py-2">
-            <tr>
+  <table className="text-base linkSection flex flex-col gap-2 py-2">
+    <tr>
 
-          
-              <Link target="_blank"
-                    rel="noopener noreferrer"
-                     href="https://github.com/deadlyunicorn/">
-                <td>
-                  <Image src="/github_filled.svg" width={20} height={20} alt='github icon'/>
-                </td>
-                <td>Github: /deadlyunicorn</td>
-              </Link>
-            </tr>
-              
-            <tr>
-              <Link target="_blank"
-                    rel="noopener noreferrer"
-                     href="https://www.linkedin.com/in/alexander-petrache/">
-                <td><Image src="/linkedin.png" width={20} height={20} alt='linkedin icon'/></td>
-                <td>Linkedin: /alexander-petrache</td>
-              </Link>
-            </tr>
-              
+  
+      <Link target="_blank"
+            rel="noopener noreferrer"
+              href="https://github.com/deadlyunicorn/">
+        <td>
+          <Image src="/github_filled.svg" width={20} height={20} alt='github icon'/>
+        </td>
+        <td>Github: /deadlyunicorn</td>
+      </Link>
+    </tr>
+      
+    <tr>
+      <Link target="_blank"
+            rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/alexander-petrache/">
+        <td><Image src="/linkedin.png" width={20} height={20} alt='linkedin icon'/></td>
+        <td>Linkedin: /alexander-petrache</td>
+      </Link>
+    </tr>
+      
 
-            <tr>
-              <Link target="_blank"
-                    rel="noopener noreferrer"
-                     href="https://www.deadlyunicorn.dev/">
-                <td>
-                  <Image id="globe" src="/web.svg" width={20} height={20} alt='globe icon'/>
-                </td>
-                <td>
-                  Personal Website
+    <tr>
+      <Link target="_blank"
+            rel="noopener noreferrer"
+              href="https://www.deadlyunicorn.dev/">
+        <td>
+          <Image id="globe" src="/web.svg" width={20} height={20} alt='globe icon'/>
+        </td>
+        <td>
+          Personal Website
 
-                </td>
-              </Link>
-            </tr>
+        </td>
+      </Link>
+    </tr>
 
-            <tr>
-              <Link href="mailto:retroalex1008@gmail.com">
+    <tr>
+      <Link href="mailto:retroalex1008@gmail.com">
 
-                <td>
-                  <Image src="/mail.svg"  width={20} height={20} alt='mail icon'/>
-                </td>
-                <td>
-                  retroalex1008@gmail.com
-                </td>
-              </Link>
-            </tr>
+        <td>
+          <Image src="/mail.svg"  width={20} height={20} alt='mail icon'/>
+        </td>
+        <td>
+          retroalex1008@gmail.com
+        </td>
+      </Link>
+    </tr>
 
-            <tr>
-              <Link href="tel:+306980234544">
-                <td>
-                  <Image src="/tel.svg" id='tel' width={20} height={20} alt='telephone icon'/>
-                </td>
-                <td>
-                  +306980234544
-                </td>
-              
-              </Link>
-            </tr>
-
-
-            <tr>
-              <Link target="_blank"
-                    rel="noopener noreferrer"
-                     href="https://goo.gl/maps/Bj8fJCYPCLUjDr1H6">
-                <td>
-                  <Image src="/house.svg" width={20} height={20} alt='house icon'/>
-                </td>
-                <td>
-                  Athens 104 44, Greece 
-
-                </td>
-              </Link>
-            </tr>
+    <tr>
+      <Link href="tel:+306980234544">
+        <td>
+          <Image src="/tel.svg" id='tel' width={20} height={20} alt='telephone icon'/>
+        </td>
+        <td>
+          +306980234544
+        </td>
+      
+      </Link>
+    </tr>
 
 
-          </table>
+    <tr>
+      <Link target="_blank"
+            rel="noopener noreferrer"
+              href="https://goo.gl/maps/Bj8fJCYPCLUjDr1H6">
+        <td>
+          <Image src="/house.svg" width={20} height={20} alt='house icon'/>
+        </td>
+        <td>
+          Athens 104 44, Greece 
+
+        </td>
+      </Link>
+    </tr>
+
+
+  </table>
 
 )
 
