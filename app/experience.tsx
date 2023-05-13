fbc7c077dc80c220480319ad4536a778213a306f
deadlyunicorn <retroalex1008@gmail.com>
2023-05-14 01:15:21 +0300

    removed underline from skills

--- app/experience.tsx
+++ app/experience.tsx
@@ -23,7 +23,7 @@ function ExperienceItem({skill,rating}:{skill:string,rating:string}){
 
   return(
     <>
-      <h2>{skill}</h2>
+      <h2 className="no-underline">{skill}</h2>
       <div className='w-full h-4 rounded-md border border-[#a786df]'>
         <div 
           className='skillBarAnimation bg-gradient-to-l from-[#a786df] to-[#d8bfff] h-full rounded-md'
