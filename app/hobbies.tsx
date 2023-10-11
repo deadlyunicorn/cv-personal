1eb81d3cd0c8b195b4870a62501c74f5a8aca76c
deadlyunicorn <retroalex1008@gmail.com>
2023-10-12 00:37:21 +0300

    additional response design fixes

--- app/hobbies.tsx
+++ app/hobbies.tsx
@@ -1,15 +1,15 @@
 export default function Hobbies(){
 
   return (
-    <div id="hobbies" className="flex w-full justify-between px-16">
+    <div id="hobbies" className="flex w-full justify-around md:flex-row flex-col items-center">
       <ul className="list-disc list-inside">
         <li>Jogging</li>
         <li>Photography</li>
         <li>Music Mixing</li>
       </ul>
       <ul className="list-roman">
-        <li>Driving License</li>
-        <p>(A1 - B)</p>
+        <li>Driving Licenses</li>
+        <p className="text-center">( A1, B )</p>
         <li>Near train station</li>
       </ul>
     </div>
